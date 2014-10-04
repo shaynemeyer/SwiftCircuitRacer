c55bcd3fb2a37d87599722f7d8a7c98e33409b87
Shayne Meyer <shayne.meyer@gmail.com>
2014-10-04 10:03:07 -0700

    finished wiring up select car and select level storyboard items.

---
+++ SwiftCircuitRacer/SelectLevelViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelectLevelViewController.swift
+//  SwiftCircuitRacer
+//
+//  Created by Shayne Meyer on 10/4/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class SelectLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
