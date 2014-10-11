b90f1e5834af03c066deeb787f7961c4bbbd84ca
Shayne Meyer <shayne.meyer@gmail.com>
2014-10-11 05:41:57 -0700

    added GameKitHelper for authenticating to GameCenter.

---
+++ SwiftCircuitRacer/GameKitHelper.swift
@@ -0,0 +1,50 @@
+//
+//  GameKitHelper.swift
+//  SwiftCircuitRacer
+//
+//  Created by Shayne Meyer on 10/11/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+import GameKit
+import Foundation
+
+let singleton = GameKitHelper()
+let PresentAuthenticationViewController = "PresentAuthenticationViewController"
+
+class GameKitHelper: NSObject {
+    var authenticationViewController: UIViewController?
+    var lastError:NSError?
+    var gameCenterEnabled: Bool
+    
+    class var sharedInstance: GameKitHelper {
+        return singleton
+    }
+    
+    override init() {
+        gameCenterEnabled = true
+        super.init()
+    }
+    
+    func authenticateLocalPlayer() {
+        //
+        let localPlayer = GKLocalPlayer.localPlayer()
+        localPlayer.authenticateHandler = {(viewController, error) in
+            // store any error in local variable
+            self.lastError = error
+            
+            if viewController != nil {
+                // if user hasnt logged into game center show game center login controller
+                self.authenticationViewController = viewController
+                
+                NSNotificationCenter.defaultCenter().postNotificationName(PresentAuthenticationViewController, object: self)
+            } else if localPlayer.authenticated {
+                // if logged in
+                self.gameCenterEnabled = true
+            } else {
+                // not logged in
+                self.gameCenterEnabled = false
+            }
+        }
+    }
+}
